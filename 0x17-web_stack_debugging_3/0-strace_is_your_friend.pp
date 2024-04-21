680c4d26e2c8dd787505d28e518c9af8bba8c69e
Alabi Olalekan <103480245+Deaconlakes@users.noreply.github.com>
2024-04-21 16:12:40 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# find out why Apache is returning a 500 error
-
-$update_file = '/var/www/html/wp-settings.php'
-
-exec { 'fixed-phpp':
-    command => "sed -i 's/phpp/php/g' ${update_file}",
-    path    => ['/bin', '/usr/bin']
-}
